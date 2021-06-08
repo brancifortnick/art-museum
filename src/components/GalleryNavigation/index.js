2c837d0c7059fd1485822712137e51db931e848f
Nicholas Brancifort <brancifortnick@yahoo.com>
2021-06-08 16:41:52 -0400

    added CSS component

--- src/components/GalleryNavigation/index.js
+++ src/components/GalleryNavigation/index.js
@@ -1,13 +1,13 @@
+import { NavLink } from 'react-router-dom';
+import "./GalleryNavigation.css";
 
-
-const GalleryNavigation = ({ galleries}) => {
-    return (
-        <nav>
-            <h1>Galleries</h1>
-        </nav>
-    )
-
-}
-
+const GalleryNavigation = ({ galleries }) => {
+  console.log(galleries);
+  return (
+    <nav>
+      <h1>Galleries</h1>
+    </nav>
+  );
+};
 
 export default GalleryNavigation;
